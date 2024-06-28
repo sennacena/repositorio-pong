989e38b89cd3a27b0b29d23a921fb8cde46ed1c4
Senna <00001093007643sp@al.educacao.sp.gov.br>
2024-06-28 08:59:45 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -133,11 +133,3 @@ function marcaPonto() {
         ponto.play();
     }
 }
-
-
-function preload(){
-  trilha = loadSound("trilha.mp3");
-  ponto = loadSound("ponto.mp3");
-  raquetada = loadSound("raquetada.mp3");
-}
-
